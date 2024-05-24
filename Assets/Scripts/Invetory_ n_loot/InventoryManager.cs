e1e30a283729fb40da1f131de622a3c94dc3e580
Nacho Medina Bernal <nachomedinabernal@gmail.com>
2024-05-24 14:39:02 +0200

    Invetory_50%

--- Assets/Scripts/Invetory_ n_loot/InventoryManager.cs
+++ Assets/Scripts/Invetory_ n_loot/InventoryManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
+    public Transform ItemContent;
+    public GameObject InventoryItem;
 
     public List<item> Items = new List<item>();
 
@@ -20,6 +25,28 @@ public class InventoryManager : MonoBehaviour
      public void RemoveItem(item item)
      {
             Items.Remove(item);
+     }
+
+
+
+     public void ListItem()
+     {
+        //
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            Debug.Log(item);
+            
+            obj.transform.Find("item_icon").GetComponent<Image>().sprite = item.iconSprite;
+            // var iconSprite = obj.transform.Find("iconSprite").GetComponent<Image>();
+            obj.transform.Find("item_name").GetComponent<TextMeshProUGUI>().text = item.itemName;
+            // iconSprite.sprite = item.iconSprite; 
+        }
      } 
 }
 
