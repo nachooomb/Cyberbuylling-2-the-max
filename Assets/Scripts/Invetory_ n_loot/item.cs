0fc32540974993a02476c0a777ee8496b4d17806
Nacho Medina Bernal <nachomedinabernal@gmail.com>
2024-05-24 12:18:26 +0200

    Invetory 11%

---
+++ Assets/Scripts/Invetory_ n_loot/item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
+public class item : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public string description;
+    public int valueDMG;
+    public Sprite iconSprite;
+
+}
