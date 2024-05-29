8bac287dfa60270b9eaec70e76d3a876cbbc293c
Nacho Medina Bernal <nachomedinabernal@gmail.com>
2024-05-29 14:27:54 +0200

    ff

--- Assets/Scripts/Invetory_ n_loot/item_prefabs/Destroy_item.cs
+++ Assets/Scripts/Invetory_ n_loot/item_prefabs/Destroy_item.cs
@@ -14,7 +14,7 @@ public class Destroy_item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TimeDestroy();
+        //TimeDestroy();
         //OnCollisionEnter(Collision col);
     }
 
