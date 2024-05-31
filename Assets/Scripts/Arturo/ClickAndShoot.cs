451f4cc8f3e31b47d062bad20c00572c79ac44b5
Nacho Medina Bernal <nachomedinabernal@gmail.com>
2024-05-31 12:28:57 +0200

    ajustes items

--- Assets/Scripts/Arturo/ClickAndShoot.cs
+++ Assets/Scripts/Arturo/ClickAndShoot.cs
@@ -11,10 +11,6 @@ public class ClickAndShoot : MonoBehaviour
     private ClickAndMove clickAndMove;
     private Transform shooting_point; 
 
-    // public Animator ArturoAnim;
-
-    // private NavMeshAgent ArturoNavMesh;
-
     void Start()
     {
         Arturo = GameObject.Find ("Arturo");
@@ -22,9 +18,6 @@ public class ClickAndShoot : MonoBehaviour
         clickAndMove = Arturo.GetComponent<ClickAndMove>();
 
         shooting_point = GameObject.Find ("shooting_point").GetComponent<Transform>();
-        
-
-        //_control_arturo.ToggleMove(); 
     }
 
     // Update is called once per frame
@@ -32,11 +25,6 @@ public class ClickAndShoot : MonoBehaviour
     {
         clickAndMove.GoDestination(shooting_point.position);
 
-        // if(ArturoNavMesh.remainingDistance>=0.1){
-        //     ArturoAnim.SetBool("walkb", true);
-        // }else{
-        //     ArturoAnim.SetBool("walkb", false);
-        // }
     }
 
 
