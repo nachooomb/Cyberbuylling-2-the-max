d89f9af98832b4747ff0f4e2f7fa1ee92cdfe9cc
nenepesaurio <boxanxsebby@gmail.com>
2024-06-04 10:42:25 +0200

    volumen

--- Assets/Scripts/audioManager.cs
+++ Assets/Scripts/audioManager.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class audioManager : MonoBehaviour
+
+
+
+
 {
+
+
+    public AudioClip bandaSonora;
+
+//   AudioSource = _audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       // gameObject.Find.videoPlayer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      // if (videoPlayer.vid.enabled = false){
+       // _audioSource = this.GetComponent<AudioSource>();
+  //  }
     }
 }
